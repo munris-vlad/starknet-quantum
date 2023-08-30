8a7ade02c3410a0b4feaad7c7432504a2fe25e6e
munris <>
2023-08-30 08:32:16 +0300

    mint enabled

--- mint.js
+++ mint.js
@@ -1,8 +1,8 @@
 import * as starknet from "starknet"
-import {readWallets, sleep} from "./common.js";
+import {random, readWallets, sleep} from "./common.js";
 import {getGroundKey, precalculateAddress, restoreSeedPhrase, starkProvider} from "./starknet.js";
 
-const quantumContract = '?'
+const quantumContract = '0x00b719f69b00a008a797dc48585449730aa1c09901fdbac1bc94b3bdc287cf76'
 
 const mnemonics = readWallets('mnemonics.txt');
 
@@ -18,15 +18,35 @@ for (let mnemonic of mnemonics) {
       starkKeyPair
     )
 
-    let callIdentity = {
+    let call = {
       contractAddress: quantumContract,
-      entrypoint: "mint?",
-      calldata: ['?'],
+      entrypoint: "mintPublic",
+      calldata: [contractAddress],
     };
 
-    let exec_hash = await account.execute([callIdentity]);
+    let mintDone = false
+    let hash
 
-    console.log(`${contractAddress}: Done -> ${exec_hash}`)
+    while (!mintDone) {
+        try {
+            let exec_hash = await account.execute([call])
+            hash = exec_hash.transaction_hash
+            mintDone = true
+        } catch (e) {
+            let err = e.toString()
+            if (err.includes('NFT already minted with this address')) {
+                console.log(`${contractAddress}: Error -> Already minted`)
+                mintDone = true
+            } else {
+                console.log(`${contractAddress}: Error -> ${e.toString()}`)
+                await sleep(15 * 1000)
+            }
+        }
+    }
 
-    await sleep(1.5 * 1000);
+    if (hash) {
+        console.log(`${contractAddress}: Done -> https://starkscan.co/tx/${hash}`)
+    }
+
+    await sleep(random(5, 8) * 1000)
 }
